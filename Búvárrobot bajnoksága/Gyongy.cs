33ca4f26b7dcf70c15b6b4f94bcc2a41d87d015a
Orosz Zsombor <101347624+Orosz-Zzsombor@users.noreply.github.com>
2024-03-20 13:08:03 +0100

    Add files via upload

---
+++ Búvárrobot bajnoksága/Gyongy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Búvárrobot_bajnoksága
+{
+    internal class Gyongy
+    {
+        int x,y,z,e;
+
+        public Gyongy(String txtsor)
+        {
+            var mezo = txtsor.Split(";");   
+            this.X = int.Parse(mezo[0]);
+            this.Y = int.Parse(mezo[1]);
+            this.Z = int.Parse(mezo[2]);
+            this.E = int.Parse(mezo[3]);
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Z { get => z; set => z = value; }
+        public int E { get => e; set => e = value; }
+
+        public double Tavolsag(Gyongy A, Gyongy B)
+        {
+            
+            double tav = Math.Sqrt(Math.Pow(B.X - A.X),2)+Math.Pow(B.Y - A.Y),2)+ (B.Z - A.Z))
+
+            return tav;
+        }
+    }
+}
---
+++ Búvárrobot bajnoksága/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using HelixToolkit.Wpf;
+using System.Windows.Media.Media3D;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Búvárrobot_bajnoksága
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Gyongy> Gyongyok = new List<Gyongy>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            File.ReadAllLines("gyongyok.txt").Skip(1).ToList().ForEach(x=> Gyongyok.Add(new Gyongy(x)));
+            foreach(var x in Gyongyok)
+            {
+                EllipsoidVisual3D gyongy3D = new EllipsoidVisual3D();
+                gyongy3D.RadiusX = 1;
+                gyongy3D.RadiusY = 1;
+                gyongy3D.RadiusZ = 1;
+                gyongy3D.Center = new Point3D(x.X, x.Y, x.Z);
+
+                gyongy3D.Fill = new SolidColorBrush(Colors.Red);
+                ter.Children.Add(gyongy3D);
+
+            }
+            
+        }
+        
+        
+
+       
+        
+    }
+}
